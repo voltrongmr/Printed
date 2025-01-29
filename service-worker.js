dd7b1eb86ae91e4191c4925e67424608b73c08e7
voltrongmr <97269298+voltrongmr@users.noreply.github.com>
2025-01-28 19:40:58 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,18 @@
-// service-worker.js
-self.addEventListener('push', function(event) {
-  const options = {
-    body: event.data ? event.data.text() : 'You have a new notification!',
-    icon: '/images/icon.png',
-    badge: '/images/badge.png'
-  };
-
-  event.waitUntil(
-    self.registration.showNotification('New Notification', options)
-  );
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
 });
 
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activated.');
+});
+
+self.addEventListener('push', function(event) {
+  var options = {
+    body: event.data.text(),
+    icon: '/path/to/icon.png',
+    badge: '/path/to/badge.png'
+  };
   event.waitUntil(
-    clients.openWindow('https://yourwebsite.com') // The URL you want to open when clicking the notification
+    self.registration.showNotification('New Notification', options)
   );
 });
